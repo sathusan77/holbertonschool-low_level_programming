7a52480a9b75a6572e7f1405c3477c8170319396
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-23 11:23:52 +0000

    Add 4-isalpha function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,13 +4,14 @@
 /**
  * File: main.h
  * Author: sathu77
- * Description: Header file containing prototypes for functions
- * used in the project functions_nested_loops.
+ * Description: En-tête contenant les prototypes des fonctions
+ * utilisées dans le projet functions_nested_loops.
  */
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 
 #endif /* MAIN_H */
