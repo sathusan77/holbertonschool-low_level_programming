f94103a50828bb7f93d69c0af27575b44150f40a
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-23 11:46:48 +0000

    Add all functions from 0 to 11 and update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,8 +4,8 @@
 /**
  * File: main.h
  * Author: sathu77
- * Description: En-tête contenant les prototypes des fonctions
- * utilisées dans le projet functions_nested_loops.
+ * Description: Header file containing prototypes for functions
+ * used in the functions_nested_loops project.
  */
 
 int _putchar(char c);
@@ -19,12 +19,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);
 
 #endif /* MAIN_H */
-
-
-
-
-
-
-
