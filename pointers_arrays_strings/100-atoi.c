537c02cee0c00417ec7eb90285e1ec664804bb5e
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-30 22:13:24 +0000

    o

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,25 +2,26 @@
 
 /**
  * _atoi - convert a string to an integer
- * @s: string to convert
+ * @s: the string to convert
  *
- * Return: integer value of string
+ * Return: the integer representation
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, result = 0;
+	int i = 0, sign = 1, num = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            result = result * 10 + (s[i] - '0');
-            if (s[i+1] < '0' || s[i+1] > '9')
-                break;
-        }
-        i++;
-    }
-    return (result * sign);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+		i++;
+	}
+
+	return (num * sign);
 }
