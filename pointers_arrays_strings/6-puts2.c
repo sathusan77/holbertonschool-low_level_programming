08a99808fe8a86b21fcecd654535c4728bb8a7dd
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-30 21:21:41 +0000

    Add puts2 function and update main.h

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
-	{
+	if (!str)
+		return;
+
+	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
+
 	_putchar('\n');
 }
