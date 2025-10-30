8c2e4eda0042db73efb4b7f4424a91967f747b29
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-30 20:54:28 +0000

    Add reset_to_98 function and update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,1 +1,13 @@
-pointers arrays strings
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Description: Header file containing prototypes for functions
+ * used in the project pointers_arrays_strings.
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
