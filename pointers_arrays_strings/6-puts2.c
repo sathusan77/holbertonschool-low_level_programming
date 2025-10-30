a34a8b2b407a57a57ad3aabb1cc5a65f34e07e2f
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-30 21:25:32 +0000

    o

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,13 @@
  */
 void puts2(char *str)
 {
-	int i;
+    int i;
 
-	if (!str)
-		return;
+    if (!str)
+        return;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+    for (i = 0; str[i] != '\0'; i += 2)
+        _putchar(str[i]);
 
-	_putchar('\n');
+    _putchar('\n');
 }
