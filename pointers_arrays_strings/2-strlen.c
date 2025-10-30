e788a9636ca58673534742c40f3ad5f9e5a496c6
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-30 20:59:32 +0000

    Add _strlen function and update main.h

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,14 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Author: sathu77
- * Description: Header file containing prototypes for functions
- * used in pointers, arrays, and strings project.
- */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif /* MAIN_H */
