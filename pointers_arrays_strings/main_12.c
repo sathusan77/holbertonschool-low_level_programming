e3215031723b8f7883a06cc9dc4de20c42fbfa9b
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-30 22:22:37 +0000

    Fix _atoi.c indentation and Betty warnings; add main_12.c test

--- pointers_arrays_strings/main_12.c
+++ pointers_arrays_strings/main_12.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-#include <limits.h>
 
 /**
- * main - check the code for _atoi with INT_MIN
+ * main - test _atoi function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    printf("%d\n", _atoi("-2147483648"));
-    return (0);
+	printf("%d\n", _atoi("-2147483648"));
+	return (0);
 }
