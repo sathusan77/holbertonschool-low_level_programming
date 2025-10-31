0a81ccf780eb522efae69203a5fc09470b740152
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-31 09:38:18 +0000

    Task 10 _atoi completed, fixed overflow handling

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,20 +8,28 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0;
+	int i = 0, sign = 1, num = 0, digit;
 
-	while (s[i] != '\0')
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			num = num * 10 + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
+		i++;
+	}
+
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	{
+		digit = s[i] - '0';
+
+		if (sign == 1 && num > (2147483647 - digit) / 10)
+			return 2147483647;
+		if (sign == -1 && num > (2147483647 - digit + 1) / 10)
+			return -2147483648;
+
+		num = num * 10 + digit;
 		i++;
 	}
 
 	return (num * sign);
 }
+
