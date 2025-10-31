0b27803a5e62d31e5cc4a1f4b9bbc6fa245cb3dc
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-10-31 09:57:12 +0000

    Task 0: _strcat completed

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	/* find end of dest */
+	while (dest[i] != '\0')
+		i++;
+
+	/* copy src to dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 int _atoi(char *s);
+char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */
