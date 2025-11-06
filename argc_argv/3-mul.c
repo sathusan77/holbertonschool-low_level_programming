72e12a9c78a75e5575225a12ad4ec98fed5e832e
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-11-06 17:59:30 +0000

    Add program to multiply two numbers from command line

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - multiplies two numbers
@@ -10,20 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-    int a, b, result;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
-    a = atoi(argv[1]);
-    b = atoi(argv[2]);
-    result = a * b;
-
-    printf("%d\n", result);
-
-    return (0);
+	return (0);
 }
 
