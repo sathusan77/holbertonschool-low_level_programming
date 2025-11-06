cb2cf0ea234bb7a7543a6a0b761341e6a2f5a188
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-11-06 18:10:55 +0000

    Add 4-add.c: sum of positive numbers, handle invalid input

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/* Prototypes pour les fonctions argc/argv */
-int is_number(char *s);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
