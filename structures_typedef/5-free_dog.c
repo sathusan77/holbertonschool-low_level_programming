51d282cfda680353c1718bcc79d63b1c2a6f4475
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-11-13 15:16:35 +0000

    o

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ */
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return;
+    if (d->name)
+        free(d->name);
+    if (d->owner)
+        free(d->owner);
+    free(d);
+}
+
