e58d6dd4bca9d2f31395d409c4185b90e832dd4f
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-11-13 00:55:12 +0000

    o

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,22 +1,22 @@
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of type dog
- * @d: pointer to the dog
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to a struct dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Description: assigns the values passed as parameters to
- *              the members of the struct dog.
+ * Description: assigns the values of name, age, and owner
+ *		to the members of the struct dog pointed to by d.
  */
 void init_dog(dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
-
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
 
