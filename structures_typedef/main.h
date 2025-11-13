41172d9dfcdee5823c705634ed9e115e5aeecaf4
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-11-13 00:57:15 +0000

    o

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,21 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * struct dog - represents a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
-struct dog
-{
-    char *name;
-    float age;
-    char *owner;
-};
+#include "dog.h"
 
-/* Prototype for init_dog */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/* Prototype */
+void init_dog(dog *d, char *name, float age, char *owner);
 
 #endif
 
