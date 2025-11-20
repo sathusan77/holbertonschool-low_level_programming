e798ec7afea5f5640055031a3683b933a9108876
jeyakumar.sathusan <jeyakumar.sathusan@gmail.com>
2025-11-20 00:07:41 +0000

    Add array_iterator function and update header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h> /* ← AJOUT OBLIGATOIRE */
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
