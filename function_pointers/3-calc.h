81f3a20fda7cc66ad45d008e7b03be1c8c197fb0
jeyakumar.sathusan <jeyakumar.sathusan@gmail.com>
2025-11-20 00:38:35 +0000

    Add calculator program with operations, operator selector and main

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,16 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
 
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+    char *op;
+    int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -20,5 +14,5 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /* CALC_H */
 
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,29 +1,24 @@
 #include "3-calc.h"
 #include <stddef.h>
 
-/**
- * get_op_func - selects correct function
- * @s: operator
- * Return: pointer to function or NULL
- */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+    op_t ops[] = {
+        { "+", op_add },
+        { "-", op_sub },
+        { "*", op_mul },
+        { "/", op_div },
+        { "%", op_mod },
+        { NULL, NULL }
+    };
+    int i = 0;
 
-	while (ops[i].op != NULL)
-	{
-		if (*(ops[i].op) == *s)
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
+    while (ops[i].op != NULL)
+    {
+        if (*(ops[i].op) == *s && s[1] == '\0')
+            return (ops[i].f);
+        i++;
+    }
+    return (NULL);
 }
 
