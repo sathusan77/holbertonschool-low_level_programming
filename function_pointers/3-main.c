1e9e370f96d76a0ccddc57f5074e5f65391506ac
jeyakumar.sathusan <jeyakumar.sathusan@gmail.com>
2025-11-21 00:08:18 +0000

    Fix indentation and remove Betty warnings

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,34 +2,41 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-    int (*op_func)(int, int);
-    int a, b;
+	int (*op_func)(int, int);
+	int a, b;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-    op_func = get_op_func(argv[2]);
-    if (!op_func)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    printf("%d\n", op_func(a, b));
-    return (0);
+	printf("%d\n", op_func(a, b));
+	return (0);
 }
 
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,8 +1,57 @@
 #include "3-calc.h"
 
-int op_add(int a, int b) { return (a + b); }
-int op_sub(int a, int b) { return (a - b); }
-int op_mul(int a, int b) { return (a * b); }
-int op_div(int a, int b) { return (a / b); }
-int op_mod(int a, int b) { return (a % b); }
+/**
+ * op_add - addition
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
 
