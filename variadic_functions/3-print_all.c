7f1e64ab606cd46d0d089188ab9d39a6a7fd85be
jeyakumar.sathusan <jeyakumar.sathusan@gmail.com>
2025-11-21 00:56:21 +0000

    o

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -19,20 +19,24 @@ void print_all(const char * const format, ...)
 
 	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == 'c')
-			printf("%s%c", sep, va_arg(args, int));
-		if (format[i] == 'i')
-			printf("%s%d", sep, va_arg(args, int));
-		if (format[i] == 'f')
-			printf("%s%f", sep, va_arg(args, double));
-		if (format[i] == 's')
+		if (format[i] == 'c' || format[i] == 'i' ||
+		    format[i] == 'f' || format[i] == 's')
 		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
+			if (format[i] == 'c')
+				printf("%s%c", sep, va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%s%d", sep, va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%s%f", sep, va_arg(args, double));
+			if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+			}
+			sep = ", ";
 		}
-		sep = ", ";
 		i++;
 	}
 
